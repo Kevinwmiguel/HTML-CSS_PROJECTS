4974a823fa82e3a7bd958102d9036813b5aebe00
Kevin Willians Miguel <kevincursos@outlook.com>
2023-05-07 00:57:17 +0100

    Update Iphone Page

--- projects/Project-6 Iphone Clone Page/js/scripts.js
+++ projects/Project-6 Iphone Clone Page/js/scripts.js
@@ -1,12 +1,12 @@
 function purple() {
-    document.getElementById("image").src = "/HTML-CSS_PROJECTS/projects/Project-6 Iphone Clone Page/img/iphone_14_pro_morado_obscuro.jpg";
+    document.getElementById("image").src = "img/iphone_14_pro_morado_obscuro.jpg";
 }
 function gold() {
-    document.getElementById("image").src = "/HTML-CSS_PROJECTS/projects/Project-6 Iphone Clone Page/img/iphone_14_pro_gold.jpg";
+    document.getElementById("image").src = "img/iphone_14_pro_gold.jpg";
 }
 function black() {
-    document.getElementById("image").src = "/HTML-CSS_PROJECTS/projects/Project-6 Iphone Clone Page/img/iphone_14_pro_black_special.jpg";
+    document.getElementById("image").src = "img/iphone_14_pro_black_special.jpg";
 }
 function white() {
-    document.getElementById("image").src = "/HTML-CSS_PROJECTS/projects/Project-6 Iphone Clone Page/img/iphone_14_pro_white.jpg";
+    document.getElementById("image").src = "img/iphone_14_pro_white.jpg";
 }
